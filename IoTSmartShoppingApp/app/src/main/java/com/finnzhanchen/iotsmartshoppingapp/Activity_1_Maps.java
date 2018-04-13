0ffcad94e58283d4d2889ed4c147ae686d44de4f
FinnZC <chenzhan1997@gmail.com>
2018-04-13 14:15:03 +0100

    Bluetooth distance visualised

--- IoTSmartShoppingApp/app/src/main/java/com/finnzhanchen/iotsmartshoppingapp/Activity_1_Maps.java
+++ IoTSmartShoppingApp/app/src/main/java/com/finnzhanchen/iotsmartshoppingapp/Activity_1_Maps.java
@@ -1,10 +1,14 @@
 package com.finnzhanchen.iotsmartshoppingapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.nfc.Tag;
+import android.location.Location;
 import android.os.Handler;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
@@ -19,6 +23,11 @@ import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -30,36 +39,47 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-public class Activity_1_Maps extends FragmentActivity implements OnMapReadyCallback {
+// AUTHOR: FINN ZHAN CHEN
+// EXTERNAL SOURCES HAVE BEEN CITED APPROPIATELY
+public class Activity_1_Maps extends AppCompatActivity
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
     private final String AUTHORISATION_BEARER = "Bearer 57:3996aa851ea17f9dd462969c686314ed878c0cf7";
-    private final String coordinateEndPointURL = "http://glenlivet.inf.ed.ac.uk:8080/api/v1/svc/ep/test_coordinate_endpoint";
-    LatLng startPosition = new LatLng(55.944433, -3.186908);
-    private LatLng currentPosition = new LatLng(55.944398, -3.186355);
-    private Circle currentPositionCircle;
+    private final String coordinateEndPointURL = "http://glenlivet.inf.ed.ac.uk:8080/api/v1/svc/ep/main";
+    LatLng startPosition = new LatLng(55.94450423292264,-3.1866753207041816);
+    private LatLng estimatedCurrentLocation = new LatLng(55.94447543468454,-3.1866848659837546);
+    private Circle estimatedCurrentLocationCircle;
     private RequestQueue queue;
+
+    // Circle represents distance derived from RSSI signals
+    // this map keeps track of the reference so the circles on map can be
+    // removed and updated when new data is received from the cloud server
+    private static HashMap<String, Circle> circlesMap = new HashMap<>();
     // Initialise beacons on a map
     private static final HashMap<String, LatLng> beaconsMap;
+
     static {
         beaconsMap = new HashMap<>();
-        beaconsMap.put("ed23c0d875cd", new LatLng(55.9444578385393,-3.1866151839494705));
-        beaconsMap.put("e7311a8eb6d7", new LatLng(55.94444244275808,-3.18672649562358860));
-        beaconsMap.put("c7bc919b2d17", new LatLng(55.94452336441765,-3.1866540759801865));
-        beaconsMap.put("ec75a5ed8851", new LatLng(55.94452261340533,-3.1867526471614838));
-        beaconsMap.put("fe12def2c943", new LatLng(55.94448393625199,-3.1868280842900276));
-        beaconsMap.put("c03b5cfa00b8", new LatLng(55.94449050761571,-3.1866483762860294));
-        beaconsMap.put("e0b83a2f802a", new LatLng(55.94443774892113,-3.1867992505431175));
-        beaconsMap.put("f15576cb0cf8", new LatLng(55.944432116316044,-3.186904862523079));
-        beaconsMap.put("f17fb178ea3d", new LatLng(55.94444938963575,-3.1869836524128914));
-        beaconsMap.put("fd8185988862", new LatLng(55.94449107087541,-3.186941407620907));
+        beaconsMap.put("ED23C0D875CD", new LatLng(55.9444578385393,-3.1866151839494705));
+        beaconsMap.put("E7311A8EB6D7", new LatLng(55.94444244275808,-3.18672649562358860));
+        beaconsMap.put("C7BC919B2D17", new LatLng(55.94452336441765,-3.1866540759801865));
+        beaconsMap.put("EC75A5AD8851", new LatLng(55.94452261340533,-3.1867526471614838));
+        beaconsMap.put("FE12DEF2C943", new LatLng(55.94448393625199,-3.1868280842900276));
+        beaconsMap.put("C03B5CFA00B8", new LatLng(55.94449050761571,-3.1866483762860294));
+        beaconsMap.put("E0B83A2F802A", new LatLng(55.94443774892113,-3.1867992505431175));
+        beaconsMap.put("F15576CB0CF8", new LatLng(55.944432116316044,-3.186904862523079));
+        beaconsMap.put("F17FB178EA3D", new LatLng(55.94444938963575,-3.1869836524128914));
+        beaconsMap.put("FD8185988862", new LatLng(55.94449107087541,-3.186941407620907));
     }
 
     @Override
@@ -70,12 +90,56 @@ public class Activity_1_Maps extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
         // For testing purposes
         testDistanceBetweenPoints();
     }
 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+    }
+
+    protected void onStart(){
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    protected void createLocationRequest() {
+        // Set the parameters for the location request
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); // preferably every 5 seconds
+        mLocationRequest.setFastestInterval(1000); // at most every second
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Can we access the user’s current location?
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates( mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        try { createLocationRequest(); }
+        catch (java.lang.IllegalStateException ise) {
+            System.out.println("IllegalStateException thrown [onConnected]");
+        }
+
         initialiseMap();
         // Initialise requestQueue for GET and POST
         // Instantiate the cache
@@ -88,22 +152,48 @@ public class Activity_1_Maps extends FragmentActivity implements OnMapReadyCallb
         queue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<StringRequest>() {
             @Override
             public void onRequestFinished(Request<StringRequest> request) {
-                updateCurrentLocation();
+                updateEstimatedCurrentLocation();
+
             }
         });
         queue.start();
-        updateCurrentLocation();
+        updateEstimatedCurrentLocation();
+
     }
 
+    @Override
+    public void onLocationChanged(Location current) {
+        System.out.println("[onLocationChanged] Lat/long now ("
+                + String.valueOf(current.getLatitude())
+                + ","
+                + String.valueOf(current.getLongitude()));
+    }
+
+    @Override
+    public void onConnectionSuspended(int flag) {
+        System.out.println(" >>>> onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle
+        // the failure silently
+        System.out.println(" >>>> onConnectionFailed");
+    }
+
+
     public void initialiseMap(){
-        setUpUI();
+        // Set up the real position from Wifi and GPS so that I can compare the difference
+        // between bluetooth estimated and Wifi & GPS estimated locations
+        setUpRealPosition();
         // Initialise start position circle
-        currentPositionCircle = mMap.addCircle(new CircleOptions()
-                .center(currentPosition)
+        estimatedCurrentLocationCircle = mMap.addCircle(new CircleOptions()
+                .center(estimatedCurrentLocation)
                 .radius(0.5) // radius of 5 metres
-                .strokeColor(Color.parseColor("#3170d6"))
-                .fillColor(0x993170d6));
-        currentPositionCircle.setZIndex(2);
+                .strokeColor(Color.parseColor("#ff0000"))
+                .fillColor(0x99ff0000));
+        estimatedCurrentLocationCircle.setZIndex(2);
         // Move camera to startPosition which is Appleton Tower
         CameraUpdate startLocationCameraUpdate = CameraUpdateFactory.newLatLngZoom(startPosition, 50);
         mMap.animateCamera(startLocationCameraUpdate);
@@ -126,22 +216,23 @@ public class Activity_1_Maps extends FragmentActivity implements OnMapReadyCallb
         handler.postDelayed(new Runnable(){
             public void run(){
                 // Update current location with a new circle
-                updateCurrentLocation();
+                updateEstimatedCurrentLocation();
                 handler.postDelayed(this, delay);
             }
         }, delay);
 
     }
 
-    public void setUpUI(){
-        // For testing purpose
-        try {
-            // Visualise current position with a small blue captureCircle
+    public void setUpRealPosition(){
+        // Can we access the user’s current location?
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
-
-
-        } catch (SecurityException se) {
-            System.out.println("Security exception thrown [onMapReady]");
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
         }
         // Add "My location" and "Zoom" button to the user interface
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
@@ -160,25 +251,36 @@ public class Activity_1_Maps extends FragmentActivity implements OnMapReadyCallb
 
     public void plotCirclesOnMap(){
         for(String macAddress : beaconsMap.keySet()){
-            mMap.addCircle(new CircleOptions()
+            Circle circle = mMap.addCircle(new CircleOptions()
                     .center(beaconsMap.get(macAddress))
-                    .radius(2) // radius of 5 metres
+                    .radius(1) // radius of 5 metres
                     .strokeColor(Color.parseColor("#3170d6"))
-                    .fillColor(0x552cdae0)).setZIndex(2);
+                    .fillColor(0x552cdae0));
+            circle.setZIndex(2);
+            circlesMap.put(macAddress, circle);
         }
     }
 
-    public void updateCurrentLocation(){
-        //Log.e("updateCurrentLocation", getRequest(queue, coordinateEndPointURL));
-        getRequest(queue, coordinateEndPointURL);
-
-        currentPositionCircle.remove();
-        currentPositionCircle = mMap.addCircle(new CircleOptions()
-                .center(currentPosition)
-                .radius(0.5) // radius of 5 metres
+    public void updateCircleOnMap(String deviceMac, float distanceReached){
+        circlesMap.get(deviceMac).remove();
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(beaconsMap.get(deviceMac))
+                .radius(distanceReached) // radius of 5 metres
                 .strokeColor(Color.parseColor("#3170d6"))
-                .fillColor(0x993170d6));
-        currentPositionCircle.setZIndex(2);
+                .fillColor(0x552cdae0));
+        circle.setZIndex(2);
+        circlesMap.put(deviceMac, circle);
+    }
+
+    public void updateEstimatedCurrentLocation(){
+        getRequest(queue, coordinateEndPointURL);
+        estimatedCurrentLocationCircle.remove();
+        estimatedCurrentLocationCircle = mMap.addCircle(new CircleOptions()
+                .center(estimatedCurrentLocation)
+                .radius(0.5) // radius of 5 metres
+                .strokeColor(Color.parseColor("#ff0000"))
+                .fillColor(0x99ff0000));
+        estimatedCurrentLocationCircle.setZIndex(2);
     }
 
     private void getRequest(RequestQueue queue, String url) {
@@ -187,10 +289,33 @@ public class Activity_1_Maps extends FragmentActivity implements OnMapReadyCallb
                     @Override
                     public void onResponse(String response) {
                         if (!response.equals(null)) {
-                            // Display the first 500 characters of the response string.
-                            String[] position = response.split(",");
-                            currentPosition = new LatLng(Float.parseFloat(position[0]), Float.parseFloat(position[1]));
-                            Log.e("getRequest", "Response is: " + response);
+                            try { // Filters responses not relevant
+                                // response is formated into
+                                // line 1 is estimated location
+                                // the rest is mac_address and its distance reached
+                                String[] lines = response.split("\\r?\\n");
+                                Log.e("getRequest", "Response is: \n" + response);
+                                if (!lines[0].equals("null")){
+                                    Log.e("lines[0]", lines[0]);
+                                    String[] estimatedLatLng = lines[0].split(",");
+                                    Log.e("getRequest", "Estimated location is " + estimatedLatLng[0] + "," + estimatedLatLng[1]);
+                                    estimatedCurrentLocation = new LatLng(Float.parseFloat(estimatedLatLng[0]), Float.parseFloat(estimatedLatLng[1]));
+
+                                } else{
+                                    Log.e("getRequest", "Location could not be estimated. ");
+                                }
+
+                                // Update beacons distance reached
+                                for (int i = 1; i < lines.length; i=i+1 ){
+                                    Log.e("lines[" + i + "]", lines[i]);
+                                    String deviceMac = lines[i].split(",")[0];
+                                    float distanceReached = Float.parseFloat(lines[i].split(",")[1]);
+                                    updateCircleOnMap(deviceMac, distanceReached);
+                                    //Log.e(deviceMac, "distance reached: "+ Float.toString(distanceReached));
+                                }
+                            }catch (Exception e){
+                                //e.printStackTrace();
+                            }
                         } else {
                             Log.e("getRequest", "Your Array Response Data Null");
                         }
@@ -261,6 +386,8 @@ public class Activity_1_Maps extends FragmentActivity implements OnMapReadyCallb
         queue.add(stringRequest);
     }
 
+    // This is given in the IoTSSC coursework description
+    // Source
     public double distanceBetweenTwoLatLng(LatLng p1, LatLng p2){
         final double R = 6381 * 1000; // Earth radius in metres
         double lat1 = p1.latitude;
